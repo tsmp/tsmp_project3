5958cf623ce50570c792fd50a3cc0562d07bbecc
maks0 <maks0_00@inbox.ru>
2020-03-17 09:10:27 +0500

    -Process_event_reject правильный код подсмотрел в OpenXray

--- xr_3da/xrGame/xrServer_process_event_reject.cpp
+++ xr_3da/xrGame/xrServer_process_event_reject.cpp
@@ -2,40 +2,78 @@
 #include "xrserver.h"
 #include "xrserver_objects.h"
 
-bool xrServer::Process_event_reject	(NET_Packet& P, const ClientID sender, const u32 time, const u16 id_parent, const u16 id_entity, bool send_message)
+bool xrServer::Process_event_reject	(NET_Packet& P, const ClientID sender, const u32 time, const u16 id_parent, 
+	const u16 id_entity, bool send_message)
 {
 	// Parse message
-	CSE_Abstract*		e_parent	= game->get_entity_from_eid	(id_parent);
-	CSE_Abstract*		e_entity	= game->get_entity_from_eid	(id_entity);
+	CSE_Abstract* e_parent = game->get_entity_from_eid(id_parent);
+	CSE_Abstract* e_entity = game->get_entity_from_eid(id_entity);
 	
 #ifdef DEBUG
 	Msg("sv reject. id_parent %s id_entity %s [%d]",ent_name_safe(id_parent).c_str(),ent_name_safe(id_entity).c_str(), Device.dwFrame);
 #endif
-	R_ASSERT			(e_parent && e_entity);
-	game->OnDetach		(id_parent,id_entity);
 
-	if (0xffff == e_entity->ID_Parent) 
+	if (!e_entity)
 	{
-		Msg	("~ ERROR: can't detach independant object. entity[%s:%d], parent[%s:%d], section[%s]",
-			e_entity->name_replace(),id_entity,e_parent->name_replace(),id_parent, *e_entity->s_name);
-		return			(false);
+		Msg("! ERROR on rejecting: entity not found. parent_id = [%d], entity_id = [%d], frame = [%d].", id_parent,
+			id_entity, Device.dwFrame);
+
+		return false;
 	}
 
-	// Rebuild parentness
-	R_ASSERT3				(e_entity->ID_Parent == id_parent, e_entity->name_replace(), e_parent->name_replace());
-	e_entity->ID_Parent		= 0xffff;
-	xr_vector<u16>& C		= e_parent->children;
-
-	xr_vector<u16>::iterator c	= std::find	(C.begin(),C.end(),id_entity);
-	R_ASSERT3				(C.end()!=c,e_entity->name_replace(),e_parent->name_replace());
-	C.erase					(c);
-
-	// Signal to everyone (including sender)
-	if (send_message)
+	if (!e_parent)
 	{
-		DWORD MODE		= net_flags(TRUE,TRUE, FALSE, TRUE);
-		SendBroadcast	(BroadcastCID,P,MODE);
+		Msg("! ERROR on rejecting: parent not found. parent_id = [%d], entity_id = [%d], frame = [%d].", id_parent,
+			id_entity, Device.dwFrame);
+
+		return false;
 	}
-	
-	return				(true);
+        
+    xr_vector<u16>& C = e_parent->children;
+    xr_vector<u16>::iterator c = std::find(C.begin(), C.end(), id_entity);
+    
+    if (c == C.end())
+    {
+        Msg("! WARNING: SV: can't find children [%s][%d] of parent [%s][%d]",e_entity->s_name.c_str(),id_entity,
+            e_parent->name_replace(),e_parent);
+
+        return false;
+    }
+
+    if (0xffff == e_entity->ID_Parent)
+    {
+        Msg("! ERROR: can't detach independant object. entity[%s][%d], parent[%s][%d], section[%s]",
+            e_entity->name_replace(), id_entity, e_parent->name_replace(), id_parent, e_entity->s_name.c_str());
+
+        return false;
+    }
+
+    // Rebuild parentness
+
+    if (e_entity->ID_Parent != id_parent)
+    {
+        Msg("! ERROR: e_entity->ID_Parent = [%d]  parent = [%d][%s]  entity_id = [%d]  frame = [%d]",
+            e_entity->ID_Parent, id_parent, e_parent->name_replace(), id_entity, Device.dwFrame);
+        
+        CSE_Abstract* e_parent_from_entity = game->get_entity_from_eid(e_entity->ID_Parent);
+
+        if (!e_parent_from_entity)
+            Msg("! ERROR: cant find parent from entity");
+        else
+            Msg("! ERROR: parent from entity is [%s][%d]",e_parent_from_entity->name_replace(), e_entity->ID_Parent);
+    }
+
+    game->OnDetach(id_parent, id_entity);
+
+    e_entity->ID_Parent = 0xffff;
+    C.erase(c);
+
+    // Signal to everyone (including sender)
+    if (send_message)
+    {
+        DWORD MODE = net_flags(TRUE, TRUE, FALSE, TRUE);
+        SendBroadcast(BroadcastCID, P, MODE);
+    }
+
+    return true;
 }
