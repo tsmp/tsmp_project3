84072b81a08e4c08dea58fc752b62fbf35ca4f93
maks0 <maks0_00@inbox.ru>
2020-03-22 10:27:41 +0500

    -SLS_Clear не падаем, поправлена сборка cpu_pipe

--- xr_3da/xrGame/xrServer_sls_clear.cpp
+++ xr_3da/xrGame/xrServer_sls_clear.cpp
@@ -6,10 +6,10 @@
 #include "xrmessages.h"
 #include "ai_space.h"
 
-void xrServer::Perform_destroy	(CSE_Abstract* object, u32 mode)
+void xrServer::Perform_destroy(CSE_Abstract* object, u32 mode)
 {
-	R_ASSERT				(object);
-	R_ASSERT				(object->ID_Parent == 0xffff);
+	R_ASSERT(object);
+	R_ASSERT(object->ID_Parent == 0xffff);
 
 #ifdef DEBUG
 #	ifdef SLOW_VERIFY_ENTITIES
@@ -17,20 +17,26 @@ void xrServer::Perform_destroy	(CSE_Abstract* object, u32 mode)
 #	endif
 #endif
 
-	while (!object->children.empty()) {
-		CSE_Abstract		*child = game->get_entity_from_eid(object->children.back());
-		R_ASSERT2			(child, make_string("child registered but not found [%d]",object->children.back()));
-//		Msg					("SLS-CLEAR : REJECT  [%s][%s] FROM [%s][%s]",child->name(),child->name_replace(),object->name(),object->name_replace());
+	while (!object->children.empty()) 
+	{
+		CSE_Abstract *child = game->get_entity_from_eid(object->children.back());
+		
+		R_ASSERT2(child, make_string("child registered but not found [%d]",object->children.back()));
+		//Msg("SLS-CLEAR : REJECT  [%s][%s] FROM [%s][%s]",child->name(),child->name_replace(),object->name(),object->name_replace());
+		
 		Perform_reject		(child,object,2*NET_Latency);
+
 #ifdef DEBUG
 #	ifdef SLOW_VERIFY_ENTITIES
 		verify_entities			();
 #	endif
 #endif
+
 		Perform_destroy		(child,mode);
 	}
 
 //	Msg						("SLS-CLEAR : DESTROY [%s][%s]",object->name(),object->name_replace());
+
 	u16						object_id = object->ID;
 	entity_Destroy			(object);
 
@@ -48,28 +54,48 @@ void xrServer::Perform_destroy	(CSE_Abstract* object, u32 mode)
 	SendBroadcast			(BroadcastCID,P,mode);
 }
 
-void xrServer::SLS_Clear		()
+void xrServer::SLS_Clear()
 {
-#if 0
-	Msg									("SLS-CLEAR : %d objects");
-	xrS_entities::const_iterator		I = entities.begin();
-	xrS_entities::const_iterator		E = entities.end();
-	for ( ; I != E; ++I)
-		Msg								("entity to destroy : [%d][%s][%s]",(*I).second->ID,(*I).second->name(),(*I).second->name_replace());
-#endif
-
-	u32									mode = net_flags(TRUE,TRUE);
-	while (!entities.empty()) {
-		bool							found = false;
-		xrS_entities::const_iterator	I = entities.begin();
-		xrS_entities::const_iterator	E = entities.end();
-		for ( ; I != E; ++I) {
+	u32	mode = net_flags(TRUE,TRUE);
+	
+	while (!entities.empty()) 
+	{
+		bool found = false;
+		
+		xrS_entities::const_iterator I = entities.begin();
+		xrS_entities::const_iterator E = entities.end();
+		
+		for ( ; I != E; ++I) 
+		{
 			if ((*I).second->ID_Parent != 0xffff)
 				continue;
+
 			found						= true;
 			Perform_destroy				((*I).second,mode);
 			break;
 		}
-		R_ASSERT						(found);
+		
+		//R_ASSERT						(found);
+	
+		if (!found)
+		{
+			I = entities.begin();
+			E = entities.end();
+
+			for (; I != E; ++I)
+			{
+				if (I->second)
+					Msg("! ERROR: can't destroy object [%d][%s] with parent [%d] section[%s]"
+						, I->second->ID
+						, I->second->s_name.size() ? I->second->s_name.c_str() : "unknown"
+						, I->second->ID_Parent
+						, I->second->s_name.c_str());
+				else
+					Msg("! ERROR: can't destroy entity [%d][?] with parent[?]", I->first);
+			}
+
+			Msg("! ERROR: FATAL: can't delete all entities !");
+			entities.clear();
+		}
 	}
 }
