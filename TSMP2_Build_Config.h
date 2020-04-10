e45940437b619d96d5c90604e1c9bc16a28abdb2
maks0 <maks0_00@inbox.ru>
2020-04-10 20:25:53 +0500

    -Заменен парсинг параметра alife на сингл Мы выяснили что в сингле игра запускается с параметрами all/single/alife/new Имя карты или спавна/ тип игры / алайф или нет / если алайф, то загружаем уровень (load) или создаем новый (new) Значит нам надо запускать с параметрами polygon/deathmatch/alife/new

--- TSMP2_Build_Config.h
+++ TSMP2_Build_Config.h
@@ -2,3 +2,4 @@
 #define DEDICATED_SERVER // Выделенный сервер
 //#define MP_LOGGING // Вывод в лог внутренних событий сервера
 #define SAVE_ERROR_REPORTS // Сохранять отчеты об ошибках 
+#define ALIFE_MP
