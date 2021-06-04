b80025cea362b637928e6e85b87f96cd6a029ad6
maks0 <maks0_00@inbox.ru>
2021-06-04 10:58:34 +0500

    Поправлены предупреждения в xrSound

--- xrSound/SoundRender_Core_Processor.cpp
+++ xrSound/SoundRender_Core_Processor.cpp
@@ -237,29 +237,33 @@ float CSoundRender_Core::get_occlusion(Fvector &P, float R, Fvector *occ)
 	range = dir.magnitude();
 	dir.div(range);
 
-	if (0 != geom_MODEL)
+	if (geom_MODEL)
 	{
 		bool bNeedFullTest = true;
 		// 1. Check cached polygon
 		float _u, _v, _range;
+
 		if (CDB::TestRayTri(base, dir, occ, _u, _v, _range, true))
+		{
 			if (_range > 0 && _range < range)
 			{
 				occ_value = psSoundOcclusionScale;
 				bNeedFullTest = false;
 			}
+		}
+
 		// 2. Polygon doesn't picked up - real database query
 		if (bNeedFullTest)
 		{
-
 			geom_DB.ray_options(CDB::OPT_ONLYNEAREST);
 			geom_DB.ray_query(geom_MODEL, base, dir, range);
-			if (0 != geom_DB.r_count())
+
+			if (geom_DB.r_count())
 			{
 				// cache polygon
-				const CDB::RESULT *R = geom_DB.r_begin();
+				const CDB::RESULT *Res = geom_DB.r_begin();
 
-				const CDB::TRI &T = geom_MODEL->get_tris()[R->id];
+				const CDB::TRI &T = geom_MODEL->get_tris()[Res->id];
 				const Fvector *V = geom_MODEL->get_verts();
 				occ[0].set(V[T.verts[0]]);
 				occ[1].set(V[T.verts[1]]);
@@ -268,22 +272,20 @@ float CSoundRender_Core::get_occlusion(Fvector &P, float R, Fvector *occ)
 			}
 		}
 	}
-	if (0 != geom_SOM)
-	{
 
+	if (geom_SOM)
+	{
 		geom_DB.ray_options(CDB::OPT_CULL);
 		geom_DB.ray_query(geom_SOM, base, dir, range);
 		u32 r_cnt = geom_DB.r_count();
-		CDB::RESULT *_B = geom_DB.r_begin();
+		CDB::RESULT* _B = geom_DB.r_begin();
 
-		if (0 != r_cnt)
+		for (u32 k = 0; k < r_cnt; k++)
 		{
-			for (u32 k = 0; k < r_cnt; k++)
-			{
-				CDB::RESULT *R = _B + k;
-				occ_value *= *(float *)&R->dummy;
-			}
+			CDB::RESULT* Res = _B + k;
+			occ_value *= *(float*)&Res->dummy;
 		}
 	}
+
 	return occ_value;
 }
