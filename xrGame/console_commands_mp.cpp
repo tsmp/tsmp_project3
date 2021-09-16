340a0f1bd87e04f48e2e571294dc44f50128419a
maks0 <maks0_00@inbox.ru>
2021-09-16 17:19:04 +0500

    Исправлен вылет при входе в меню сетевой после предыдущей правки

--- xrGame/console_commands_mp.cpp
+++ xrGame/console_commands_mp.cpp
@@ -2134,7 +2134,7 @@ void register_mp_console_commands()
 	CMD4(CCC_SV_Integer, "sv_auto_team_swap", (int *)&g_sv_tdm_bAutoTeamSwap, 0, 1);
 	CMD4(CCC_SV_Integer, "sv_friendly_indicators", (int *)&g_sv_tdm_bFriendlyIndicators, 0, 1);
 	CMD4(CCC_SV_Integer, "sv_friendly_names", (int *)&g_sv_tdm_bFriendlyNames, 0, 1);
-	CMD4(CCC_SV_Integer, "sv_friendlyfire", (int*)&g_sv_tdm_bFriendlyFireEnabled, 0, 1);
+	CMD4(CCC_SV_Integer, "sv_friendly_fire", (int*)&g_sv_tdm_bFriendlyFireEnabled, 0, 1);
 	CMD4(CCC_SV_Integer, "sv_teamkill_limit", &g_sv_tdm_iTeamKillLimit, 0, 100);
 	CMD4(CCC_SV_Integer, "sv_teamkill_punish", (int *)&g_sv_tdm_bTeamKillPunishment, 0, 1);
 
