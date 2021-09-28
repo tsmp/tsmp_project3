95cfd5cf8d2a0ce0962b2307d38dceed9d9798be
maks0 <maks0_00@inbox.ru>
2021-09-28 19:57:41 +0500

    Поправлено отображение названия режима hardmatch на сервере

--- xrGame/game_sv_hardmatch.h
+++ xrGame/game_sv_hardmatch.h
@@ -7,6 +7,8 @@ class game_sv_Hardmatch : public game_sv_Deathmatch
 	CRandom m_WeaponsRandom;
 
 public:
+	game_sv_Hardmatch() { m_type = GAME_HARDMATCH; }
+
 	virtual LPCSTR type_name() const override { return "hardmatch"; };
 
 	virtual void SpawnWeaponsForActor(CSE_Abstract* pE, game_PlayerState* ps) override;
