2e4048b5411c2f276c9761791d3b696e80ef0600
maks0 <maks0_00@inbox.ru>
2022-01-24 21:46:08 +0500

    Попытка исправить вылет при убирании нпс пистолетов

--- xrGame/object_handler.cpp
+++ xrGame/object_handler.cpp
@@ -170,7 +170,11 @@ void CObjectHandler::weapon_bones(int &b0, int &b1, int &b2) const
 		return;
 	}
 
-	THROW3(weapon->can_be_strapped(), "Cannot strap weapon", *weapon->cName());
+	if (!weapon->can_be_strapped())
+	{
+		Msg("! error, cant strap weapon %s", weapon->cName().c_str());
+		return;
+	}
 
 	if (weapon->ID() != m_strap_object_id)
 	{
