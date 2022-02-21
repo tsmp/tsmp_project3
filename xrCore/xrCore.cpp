4269c425752a6eb82f5da7b95d7fcacf5ec9875d
maks0 <maks0_00@inbox.ru>
2022-02-21 21:20:32 +0500

    Починена физика на x64

--- xrCore/xrCore.cpp
+++ xrCore/xrCore.cpp
@@ -139,7 +139,11 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD ul_reason_for_call, LPVOID lpvRese
 	case DLL_PROCESS_ATTACH:
 	{
 		_clear87();
+
+#ifndef _WIN64 // unsupported on x64
 		_control87(_PC_53, MCW_PC);
+#endif
+
 		_control87(_RC_CHOP, MCW_RC);
 		_control87(_RC_NEAR, MCW_RC);
 		_control87(_MCW_EM, MCW_EM);
