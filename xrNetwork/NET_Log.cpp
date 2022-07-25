1ea7c3025fd43719c38cef2111da5a87a19f72f9
VAX325 <48758496+VAX325@users.noreply.github.com>
2022-07-25 18:29:11 +0400

    Починил вылет при запуске сервера в клиенте. Добавил новые типы пакетов в NET_Log. Добавил в него же поддержку пакетов с timestamp'ом в заголовке.

--- xrNetwork/NET_Log.cpp
+++ xrNetwork/NET_Log.cpp
@@ -39,12 +39,11 @@ string64 PacketName[] = {
 	"M_CLIENT_REQUEST_CONNECTION_DATA",
 
 	"M_CHAT_MESSAGE",
+	"M_CLIENT_WARN",
 	"M_CHANGE_LEVEL_GAME",
 	//-----------------------------------------------------
 	"M_CL_PING_CHALLENGE",
 	"M_CL_PING_CHALLENGE_RESPOND",
-	//-----------------------------------------------------
-	"M_PAUSE_GAME",
 	//-----------------------------------------------------
 	"M_AUTH_CHALLENGE",
 	"M_CL_AUTH",
@@ -57,9 +56,19 @@ string64 PacketName[] = {
 	//-----------------------------------------------------
 	"M_MOVE_PLAYERS",
 	"M_MOVE_PLAYERS_RESPOND",
+	"M_CHANGE_SELF_NAME",
+	"M_REMOTE_CONTROL_AUTH",
+	"M_REMOTE_CONTROL_CMD",
+	"M_BATTLEYE",
+	"M_MAP_SYNC",
+	"M_HW_CHALLENGE",
+	"M_HW_RESPOND",
+	"M_FILE_TRANSFER",
 
-	"MSG_FORCEDWORD"};
-//---------------------------------------------------------
+	"MSG_FORCEDWORD"
+};
+constexpr size_t PacketTypeSize = sizeof(PacketName) / sizeof(PacketName[0]);
+
 INetLog::INetLog(LPCSTR sFileName, u32 dwStartTime)
 #ifdef PROFILE_CRITICAL_SECTIONS
 	: m_cs(MUTEX_PROFILE_ID(NET_Log))
@@ -89,7 +98,7 @@ void INetLog::FlushLog()
 		for (xr_vector<SLogPacket>::iterator it = m_aLogPackets.begin(); it != m_aLogPackets.end(); it++)
 		{
 			SLogPacket *pLPacket = &(*it);
-			if (pLPacket->m_u16Type >= sizeof(PacketName) / sizeof(PacketName[0]))
+			if (pLPacket->m_u16Type >= PacketTypeSize)
 			{
 				sprintf(buff, "%s %10d %10d %10d", pLPacket->m_bIsIn ? "In: " : "Out:", pLPacket->m_u32Time, pLPacket->m_u16Type, pLPacket->m_u32Size);
 			}
@@ -115,6 +124,18 @@ void INetLog::LogPacket(u32 Time, NET_Packet *pPacket, bool IsIn)
 	SLogPacket NewPacket;
 
 	NewPacket.m_u16Type = *((u16 *)&pPacket->B.data);
+	if (NewPacket.m_u16Type >= PacketTypeSize)
+	{
+		u32 timestamp = 0;
+		pPacket->r_u32(timestamp);
+
+		u16 type;
+		pPacket->r_u16(type);
+
+		if (!(type >= PacketTypeSize))
+			NewPacket.m_u16Type = type;
+	}
+
 	NewPacket.m_u32Size = pPacket->B.count;
 	NewPacket.m_u32Time = Time - m_dwStartTime;
 	NewPacket.m_bIsIn = IsIn;
@@ -136,6 +157,14 @@ void INetLog::LogData(u32 Time, void *data, u32 size, bool IsIn)
 	SLogPacket NewPacket;
 
 	NewPacket.m_u16Type = *((u16 *)data);
+	if (NewPacket.m_u16Type >= PacketTypeSize)
+	{
+		u16 type = *((u16*)&(((char*)data)[sizeof(u32)]));
+
+		if (!(type >= PacketTypeSize))
+			NewPacket.m_u16Type = type;
+	}
+
 	NewPacket.m_u32Size = size;
 	NewPacket.m_u32Time = Time - m_dwStartTime;
 	NewPacket.m_bIsIn = IsIn;
