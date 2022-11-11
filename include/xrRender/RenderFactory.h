435014c834115a286462971ad9b2ab92a91deffe
maks0 <maks0_00@inbox.ru>
2022-11-11 17:24:04 +0500

    Добавлен интерфейс UIShader

--- include/xrRender/RenderFactory.h
+++ include/xrRender/RenderFactory.h
@@ -30,7 +30,7 @@ class IRenderFactory
 {
 public:
 	//RENDER_FACTORY_INTERFACE(UISequenceVideoItem)
-	//RENDER_FACTORY_INTERFACE(UIShader)
+	RENDER_FACTORY_INTERFACE(UIShader)
 	//RENDER_FACTORY_INTERFACE(StatGraphRender)
 	RENDER_FACTORY_INTERFACE(RenderDeviceRender)
 
