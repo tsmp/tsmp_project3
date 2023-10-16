c766b114457d950f5256b341cdb8226bb02dcde4
maks0 <maks0_00@inbox.ru>
2023-10-16 22:12:35 +0500

    Исправлен краш при чтении битого user.ltx
    По какой-то причине после игры бывает что user.ltx имеет размер как нормальный, но сам весь заполнен нулями. А функция чтения строки ожидает что терминаторами строки будут 10 и 13. В таком случае чтение выходит за границу буфера размером 1024 и срабатывает ассерт. Будем использовать xr_string и читать все что есть, получится строка нулевой длины. Настройки будут сброшены до дефолтных, ведь их взять негде, зато без крашей.

--- xrEngine/Console_commands.cpp
+++ xrEngine/Console_commands.cpp
@@ -194,18 +194,15 @@ void CCC_LoadCFG::Execute(LPCSTR args)
 	if (NULL == FS.exist(cfg_full_name))
 		strcpy_s(cfg_full_name, cfg_name);
 
-	IReader *F = FS.r_open(cfg_full_name);
-
-	string1024 str;
-
-	if (F)
+	if (IReader* F = FS.r_open(cfg_full_name))
 	{
 		while (!F->eof())
 		{
-			F->r_string(str, sizeof(str));
+			xr_string str;
+			F->r_string(str);
 
-			if (allow(str))
-				Console->Execute(str);
+			if (allow(str.c_str()))
+				Console->Execute(str.c_str());
 		}
 
 		FS.r_close(F);
