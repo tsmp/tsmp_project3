443e4372ba0dc8d4055aa426adc7130054c34a03
maks0 <maks0_00@inbox.ru>
2023-11-08 23:43:42 +0500

    Добавлен ключ, убирающий границу окна

--- xrEngine/HW.cpp
+++ xrEngine/HW.cpp
@@ -447,7 +447,11 @@ void CHW::updateWindowProps(HWND m_hWnd)
 	// Set window properties depending on what mode were in.
 	if (bWindowed)
 	{
-		SetWindowLong(m_hWnd, GWL_STYLE, dwWindowStyle = (WS_BORDER | WS_DLGFRAME | WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX));
+		if (strstr(Core.Params, "-no_dialog_header"))
+			SetWindowLong(m_hWnd, GWL_STYLE, dwWindowStyle = (WS_BORDER | WS_VISIBLE));
+		else
+			SetWindowLong(m_hWnd, GWL_STYLE, dwWindowStyle = (WS_BORDER | WS_DLGFRAME | WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX));
+
 		// When moving from fullscreen to windowed mode, it is important to
 		// adjust the window size after recreating the device rather than
 		// beforehand to ensure that you get the window size you want.  For
