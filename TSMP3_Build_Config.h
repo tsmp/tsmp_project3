f8445e705d25fa23df1a1234e5bce17dee749876
nkliev <nkliev@bk.ru>
2024-02-05 18:41:36 +0300

    раскоментирован FZ_MOD_CLIENT

--- TSMP3_Build_Config.h
+++ TSMP3_Build_Config.h
@@ -7,7 +7,7 @@
 #endif
 //#define PUBLIC_BUILD // Публичная сборка, запрещены читерские отладочные команды
 //#define MP_LOGGING // Вывод в лог внутренних событий сервера
-//#define FZ_MOD_CLIENT // Мод, скачиваемый игроку с помощью fz
+#define FZ_MOD_CLIENT // Мод, скачиваемый игроку с помощью fz
 //#define NO_SINGLE // Запретить запускать сингл
 //#define NO_MULTI_INSTANCES // Запретить запускать несколько экземпляров игры
 //#define EVERYBODY_IS_ENEMY // Все нпс считают всех врагами
