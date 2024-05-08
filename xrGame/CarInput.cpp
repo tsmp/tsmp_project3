073a166ff2c05b25e470d6a94536f46b9b1b8aa3
nkliev <nkliev@bk.ru>
2024-05-08 16:13:21 +0300

    исправлено отсутствие возможности достать оружие после того как актор сел в транспорт

--- xrGame/CarInput.cpp
+++ xrGame/CarInput.cpp
@@ -155,7 +155,6 @@ void CCar::OnChangeLookout(bool enabled)
 	}
 
 	auto& ownerInv = ownerAct->inventory();
-	ownerInv.SetSlotsBlocked(static_cast<u16>(INV_STATE_BLOCK_ALL), !enabled);
 
 	if (!enabled)
 		ownerInv.SetActiveSlot(NO_ACTIVE_SLOT);
